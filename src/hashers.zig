fd0db6df7d96f1ea6bf6e6474b6784ea2aa31628
Wannes Vantorre <vantorrewannes@gmail.com>
2026-06-09 14:13:32 +0200

    Enforce size constraint in NumberHasher
    Ensure that the bit size of the input data does not exceed the bit size
    of the resulting hash to maintain integrity.

--- src/hashers.zig
+++ src/hashers.zig
@@ -1,6 +1,7 @@
 const std = @import("std");
 
 pub fn NumberHasher(comptime Data: type, comptime Hash: type) type {
+    if (@bitSizeOf(Data) < @bitSizeOf(Hash)) @compileError("Data type cannot be larger than Hash type");
     return struct {
         const Self = @This();
         const PRIME = switch (Data) {
@@ -33,31 +34,21 @@ pub fn NumberHasher(comptime Data: type, comptime Hash: type) type {
 test {
     {
         std.testing.refAllDecls(NumberHasher(u8, u8));
-        std.testing.refAllDecls(NumberHasher(u8, u16));
-        std.testing.refAllDecls(NumberHasher(u8, u32));
-        std.testing.refAllDecls(NumberHasher(u8, u64));
-        std.testing.refAllDecls(NumberHasher(u8, u128));
     }
     {
         std.testing.refAllDecls(NumberHasher(u16, u8));
         std.testing.refAllDecls(NumberHasher(u16, u16));
-        std.testing.refAllDecls(NumberHasher(u16, u32));
-        std.testing.refAllDecls(NumberHasher(u16, u64));
-        std.testing.refAllDecls(NumberHasher(u16, u128));
     }
     {
         std.testing.refAllDecls(NumberHasher(u32, u8));
         std.testing.refAllDecls(NumberHasher(u32, u16));
         std.testing.refAllDecls(NumberHasher(u32, u32));
-        std.testing.refAllDecls(NumberHasher(u32, u64));
-        std.testing.refAllDecls(NumberHasher(u32, u128));
     }
     {
         std.testing.refAllDecls(NumberHasher(u64, u8));
         std.testing.refAllDecls(NumberHasher(u64, u16));
         std.testing.refAllDecls(NumberHasher(u64, u32));
         std.testing.refAllDecls(NumberHasher(u64, u64));
-        std.testing.refAllDecls(NumberHasher(u64, u128));
     }
     {
         std.testing.refAllDecls(NumberHasher(u128, u8));
--- src/root.zig
+++ src/root.zig
@@ -1,6 +1,8 @@
 const std = @import("std");
 const luts = @import("luts.zig");
+const hashers = @import("hashers.zig");
 
 test {
     std.testing.refAllDecls(luts);
+    std.testing.refAllDecls(hashers);
 }
